9f738cc2c4e7b648eb4d9beff517a57c1cc7f4b4
dhia7 <dhianaija@gmail.com>
2024-12-18 14:24:49 +0100

    New commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -110,17 +110,10 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string) {
 
-  try{
+export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
-  } catch (error) {
-    return {
-      message: 'Database Error: Failed to Delete Invoice.',
-    };
-  }
-
 }
 
 export async function authenticate(
